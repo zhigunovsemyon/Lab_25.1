be9388d925b9eafb22b9508e4bb542d542e97fc4
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-06-02 19:48:05 +0300

    Составил алгоритм программы

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
-#include "queue.h"
+#include "queue.h"/*<stdio.h> <malloc.h> <stdint.h>*/
+
 /* Дан файл из целых чисел.Используя очередь,
 за один просмотр файла напечатать сначала все отрицательные числа,
 затем все положительные числа, сохраняя исходный порядок в каждой группе */
@@ -17,6 +18,28 @@ int main(int argc, const char *args[])
 		return 1;
 	}
 
+	queue *q_positive = QueueInit();
+	queue *q_negative = QueueInit();
+
+	for (int8_t buf;												//Буфер
+		fscanf(source, "%hhd", &buf) > 0;							//Чтение из файла в буфер
+		QueuePushToEnd(buf, (buf < 0) ? q_negative : q_positive));	//Добавление из буфера в очередь
+
 	fclose(source);
+
+	puts("Очередь положительных чисел:");
+	for (int8_t buf;				//Буфер
+		!QueuePullFromHead(&buf, q_positive); 	//Чтение из очереди в буфер
+		printf("%hhd ", buf));		//Вывод из буфера на экран
+	putchar('\n');
+
+	puts("Очередь отрицательных чисел :");
+	for (int8_t buf;						   // Буфер
+		 !QueuePullFromHead(&buf, q_negative); // Чтение из очереди в буфер
+		 printf("%hhd ", buf)); // Вывод из буфера на экран
+	putchar('\n');
+
+	QueueFree(&q_positive);
+	QueueFree(&q_negative);
 	return 0;
 }
