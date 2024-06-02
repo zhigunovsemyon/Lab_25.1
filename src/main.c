47c98c50a120d689ab40fa4336acd816b894cbe3
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-06-02 19:26:48 +0300

    Осуществляется открытие файла

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include "queue.h"
-
-int main(void)
+/* Дан файл из целых чисел.Используя очередь,
+за один просмотр файла напечатать сначала все отрицательные числа,
+затем все положительные числа, сохраняя исходный порядок в каждой группе */
+int main(int argc, const char *args[])
 {
-	puts("kek");
+	if (argc < 2)
+	{
+		puts("Не было передано имя файла!");
+		return 1;
+	}
+
+	FILE *source = fopen(args[1], "rt");
+	if (!source)
+	{
+		perror(args[1]);
+		return 1;
+	}
+
+	fclose(source);
 	return 0;
 }
