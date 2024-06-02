633b132ff10e589754280d33c16b134549e2d4c5
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-06-02 19:18:07 +0300

    Создал описание типов данных

--- src/queue.h
+++ src/queue.h
@@ -2,6 +2,21 @@
 #define _QUEUE_H
 
 #include <stdio.h>
+#include <malloc.h>
+#include <stdint.h>
+
+struct part
+{
+	int8_t num;
+	struct part *next;
+};
+
+typedef struct 
+{
+	struct part *head;
+	struct part *tail;
+}
+queue;
 
 #endif // !_QUEUE_H
 
